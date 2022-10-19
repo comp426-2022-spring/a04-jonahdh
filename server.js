a2f63f762d82bed53433a0b5ad2faf78856eef1c
jonahdh <jonahdh@email.unc.edu>
2022-10-19 08:40:30 -0400

    Chaged to type: module, brought coin module, added help text

--- server.js
+++ server.js
@@ -9,6 +9,29 @@ args['port'];
 const HTTP_PORT = args.port || 5000;
 const app = express();
 
+// Help message + routine if --help is flagged
+const help = (`
+server.js [options]
+
+--port	Set the port number for the server to listen on. Must be an integer
+            between 1 and 65535.
+
+--debug	If set to true, creates endlpoints /app/log/access/ which returns
+            a JSON access log from the database and /app/error which throws 
+            an error with the message "Error test successful." Defaults to 
+            false.
+
+--log		If set to false, no log files are written. Defaults to true.
+            Logs are always written to database.
+
+--help	Return this message and exit.
+`)
+// If --help or -h, echo help text to STDOUT and exit
+if (args.help || args.h) {
+    console.log(help)
+    process.exit(0)
+}
+
 // Start an app server
 const server = app.listen(HTTP_PORT, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%',HTTP_PORT))
